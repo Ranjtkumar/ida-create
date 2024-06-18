b44ea57b840639c254edad4be584cfa5e67eb763
Ranjitkumar <ranjitkumrm5427@gmail.com>
2024-06-18 17:46:54 +0530

    changes

---
+++ ida-front-end/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import './index.css'
+
+function App() {
+  return (
+    <div className='font-bold text-center'>
+      Hello World
+    </div>
+  );
+}
+
+export default App;
